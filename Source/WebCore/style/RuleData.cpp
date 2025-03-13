d6f4a9e9a6cb3d6267507959c178302ee2ffa9b1
Tim Nguyen <ntim@apple.com>
2025-03-13 13:18:12 -0700

    [appearance: base] Add style switching mechanism for UA stylesheets https://bugs.webkit.org/show_bug.cgi?id=278084 rdar://133818717
    Reviewed by Antti Koivisto.
    
    This adds a `@-internal-base-appearance` at-rule that can only be used in UA stylesheets.
    
    Rules contained inside that at-rule will only be applied when `appearance: base` is set (see `html.css`).
    
    * Source/WebCore/Sources.txt:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebCore/bindings/js/JSCSSRuleCustom.cpp:
    (WebCore::toJSNewlyCreated):
    * Source/WebCore/css/CSSInternalBaseAppearanceRule.cpp: Added.
    (WebCore::CSSInternalBaseAppearanceRule::CSSInternalBaseAppearanceRule):
    (WebCore::CSSInternalBaseAppearanceRule::cssText const):
    * Source/WebCore/css/CSSInternalBaseAppearanceRule.h: Added.
    * Source/WebCore/css/StyleRule.cpp:
    (WebCore::StyleRuleBase::visitDerived):
    (WebCore::StyleRuleBase::createCSSOMWrapper const):
    (WebCore::StyleRuleInternalBaseAppearance::create):
    (WebCore::StyleRuleInternalBaseAppearance::StyleRuleInternalBaseAppearance):
    * Source/WebCore/css/StyleRule.h:
    (WebCore::StyleRuleBase::isGroupRule const):
    (WebCore::StyleRuleBase::isInternalBaseAppearanceRule const):
    (isType):
    * Source/WebCore/css/StyleRuleType.h:
    * Source/WebCore/css/StyleSheetContents.cpp:
    (WebCore::StyleSheetContents::traverseSubresources const):
    (WebCore::StyleSheetContents::mayDependOnBaseURL const):
    * Source/WebCore/css/html.css:
    * Source/WebCore/css/parser/CSSAtRuleID.cpp:
    (WebCore::cssAtRuleID):
    * Source/WebCore/css/parser/CSSAtRuleID.h:
    * Source/WebCore/css/parser/CSSParserImpl.cpp:
    (WebCore::CSSParserImpl::consumeAtRule):
    (WebCore::CSSParserImpl::consumeInternalBaseAppearanceRule):
    * Source/WebCore/css/parser/CSSParserImpl.h:
    * Source/WebCore/css/parser/CSSPropertyParser.cpp:
    (WebCore::CSSPropertyParser::parseValue):
    (WebCore::CSSPropertyParser::parseInternalBaseAppearanceDescriptor):
    * Source/WebCore/css/parser/CSSPropertyParser.h:
    * Source/WebCore/inspector/InspectorStyleSheet.cpp:
    (WebCore::flatteningStrategyForStyleRuleType):
    * Source/WebCore/style/ElementRuleCollector.cpp:
    (WebCore::Style::ElementRuleCollector::transferMatchedRules):
    (WebCore::Style::ElementRuleCollector::addMatchedProperties):
    * Source/WebCore/style/MatchResult.h:
    (WebCore::Style::operator==):
    (WebCore::Style::MatchResult::cacheablePropertiesEqual const):
    (WebCore::Style::add):
    * Source/WebCore/style/PropertyCascade.cpp:
    (WebCore::Style::PropertyCascade::addMatch):
    (WebCore::Style::PropertyCascade::addBaseAppearanceStyles):
    * Source/WebCore/style/PropertyCascade.h:
    * Source/WebCore/style/RuleData.cpp:
    (WebCore::Style::RuleData::RuleData):
    * Source/WebCore/style/RuleData.h:
    (WebCore::Style::RuleData::usedRuleTypes const):
    (WebCore::Style::RuleData::isStartingStyle const): Deleted.
    * Source/WebCore/style/RuleFeature.cpp:
    (WebCore::Style::RuleFeatureSet::collectFeatures):
    * Source/WebCore/style/RuleSet.cpp:
    (WebCore::Style::RuleSet::addRule):
    * Source/WebCore/style/RuleSetBuilder.cpp:
    (WebCore::Style::RuleSetBuilder::addChildRule):
    (WebCore::Style::RuleSetBuilder::addStyleRuleWithSelectorList):
    * Source/WebCore/style/RuleSetBuilder.h:
    * Source/WebCore/style/StyleBuilder.cpp:
    (WebCore::Style::Builder::Builder):
    * Source/WebCore/style/StyleBuilder.h:
    * Source/WebCore/style/StyleTreeResolver.cpp:
    (WebCore::Style::TreeResolver::resolveStartingStyle):

--- Source/WebCore/style/RuleData.cpp
+++ Source/WebCore/style/RuleData.cpp
@@ -166,17 +166,17 @@ static inline PropertyAllowlist determinePropertyAllowlist(const CSSSelector* se
     return PropertyAllowlist::None;
 }
 
-RuleData::RuleData(const StyleRule& styleRule, unsigned selectorIndex, unsigned selectorListIndex, unsigned position, IsStartingStyle isStartingStyle)
+RuleData::RuleData(const StyleRule& styleRule, unsigned selectorIndex, unsigned selectorListIndex, unsigned position, OptionSet<UsedRuleType> usedRuleTypes)
     : m_styleRuleWithSelectorIndex(&styleRule, static_cast<uint16_t>(selectorIndex))
     , m_selectorListIndex(selectorListIndex)
-    , m_position(position)
     , m_matchBasedOnRuleHash(enumToUnderlyingType(computeMatchBasedOnRuleHash(*selector())))
     , m_canMatchPseudoElement(selectorCanMatchPseudoElement(*selector()))
     , m_containsUncommonAttributeSelector(computeContainsUncommonAttributeSelector(*selector()))
     , m_linkMatchType(SelectorChecker::determineLinkMatchType(selector()))
     , m_propertyAllowlist(enumToUnderlyingType(determinePropertyAllowlist(selector())))
-    , m_isStartingStyle(enumToUnderlyingType(isStartingStyle))
+    , m_usedRuleTypes(usedRuleTypes.toRaw())
     , m_isEnabled(true)
+    , m_position(position)
     , m_descendantSelectorIdentifierHashes(SelectorFilter::collectHashes(*selector()))
 {
     ASSERT(m_position == position);
--- Source/WebCore/style/StyleTreeResolver.cpp
+++ Source/WebCore/style/StyleTreeResolver.cpp
@@ -817,7 +817,7 @@ ElementUpdate TreeResolver::createAnimatedElementUpdate(ResolvedStyle&& resolved
 
 std::unique_ptr<RenderStyle> TreeResolver::resolveStartingStyle(const ResolvedStyle& resolvedStyle, const Styleable& styleable, const ResolutionContext& resolutionContext)
 {
-    if (!resolvedStyle.matchResult || !resolvedStyle.matchResult->hasStartingStyle)
+    if (!resolvedStyle.matchResult || !resolvedStyle.matchResult->usedRuleTypes.contains(UsedRuleType::StartingStyle))
         return nullptr;
 
     // "Starting style inherits from the parent’s after-change style just like after-change style does."
