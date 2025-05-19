37ecb57932effb2f5a34b26a06364f8143d9d793
Zak Ridouh <zakr@apple.com>
2025-05-19 03:28:44 -0500

    Prune leaf PlatformCA layers from the CoreAnimation tree when possible https://bugs.webkit.org/show_bug.cgi?id=293218 rdar://151592961
    Reviewed by NOBODY (OOPS!).
    
    WIP
    
    In some scenarios, similar to when we detach backing stores for some layers,
    we can get away with removing some platform layers from the CA tree.
    
    Some situations where this can be done:
    - off screen leaf layers that aren't involved in complex 3D transformations or animations
    - any layer that we have decided doesn't need a backing store is generally safe to prune from the tree
    
    We should remove these from the tree and not commit them to CoreAnimation whenever possible.
    
    Local testing shows a big drop in number of layers we're committing to CoreAnimation.
    
    * Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp:
    (WebCore::GraphicsLayerCA::commitLayerChangesBeforeSublayers):
    (WebCore::GraphicsLayerCA::updateSublayerList):
    (WebCore::GraphicsLayerCA::ensureStructuralLayer):
    * Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h:

--- Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
+++ Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -722,6 +722,8 @@ private:
     FloatSize m_sizeAtLastCoverageRectUpdate;
     FloatSize m_pixelAlignmentOffset;
 
+    bool m_isPrunedFromCATree { false };
+
     Color m_contentsSolidColor;
 
     TileCoverage m_tileCoverage;
